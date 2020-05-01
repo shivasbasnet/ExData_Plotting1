21a3f0b5468d9b73f1b14bbca543fcf6620f5519
shivasbasnet <gorkhalishiva@gmail.com>
2020-04-30 19:28:29 -0500

    Plot1

--- Plot1.R
+++ Plot1.R
@@ -1,3 +1,28 @@
-# My project
+#Home work 1
+# Plot 1
+# Read data from the file
 
-print("Project 1")
+library(dplyr)
+
+mydata <- read.table("household_power_consumption.txt", sep=";", header=TRUE, stringsAsFactors=FALSE)
+
+head(mydata)
+
+mysubset<-subset(mydata, (Date=="2/1/2007") | (Date=="2/2/2007"))
+
+mysubset<- mutate(mysubset, GAP=as.numeric(Global_active_power))
+
+
+png(filename = "plot1.png",width = 480, height = 480)
+
+hist(mysubset$GAP, 
+     col = "red", 
+     xlab = "Global Active Power (kilowatts)",
+     main = "Global Active Power")
+
+
+dev.off()
+
+
+
+
